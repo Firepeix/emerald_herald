3d711c5bf964b21bc4b22c580e37285a4cb24dec
Arthur Fernandes <arthurhmf@gmail.com>
2022-11-29 19:57:59 -0300

    feat: release v0.1.0

--- src/date/mod.rs
+++ src/date/mod.rs
@@ -13,6 +13,6 @@ impl Serialize for DateTime {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: serde::Serializer {
-        serializer.serialize_str(&format!("{}", self.0).split('.').next().unwrap_or("NOT A DATE"))
+        serializer.serialize_str(format!("{}", self.0).split('.').next().unwrap_or("NOT A DATE"))
     }
 }
